5d83426a568f1b1b01a9a32049c52d4103d5c235
mwangii <jlmwangi254@gmail.com>
2024-04-29 12:32:38 +0300

    handle api status

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """calls the storage.close method"""
+    storage.close()
+
+if __name__ == "__main__":
+    import os
+    hbnb_api_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    hbnb_api_port = int(os.getenv('HBNB_API_PORT', 5000))
+
+    app.run(host=hbnb_api_host, port=hbnb_api_port, threaded=True)

2677834d03c5d9c0d7e322f589e61d68ff4d50fe
mwangii <jlmwangi254@gmail.com>
2024-05-02 07:45:19 +0300

    update places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,10 +2,11 @@
 """creates a new view for place objects"""
 
 from api.vi.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 from models import storage
 from models.city import City
 from models.place import Place
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
@@ -43,3 +44,49 @@ def delete_place(place_id):
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'])
+def create_place(city_id):
+    """creates a place object"""
+    body = request.get_json()
+    if not body:
+        abort(400, "Not a JSON")
+
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    if 'user_id' not in body:
+        abort(400, "Missing user_id")
+
+    if 'name' not in body:
+        abort(400, "Missing name")
+
+    user_id = body['user_id']
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    new_place = Place(city_id=city_id, user_id=user_id, name=body['name'])
+    storage.add(new_place)
+    storage.save()
+
+    return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'])
+def update_place(place_id):
+    """updates a place in the storage"""
+    body = request.get_json()
+    if not body:
+        abort(400, "Not a JSON")
+
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+
+    for key, value in body.items():
+        if hasattr(place, key):
+            setattr(place, key, value)
+
+    storage.save()
+
+    return jsonify(place.to_dict()), 200
